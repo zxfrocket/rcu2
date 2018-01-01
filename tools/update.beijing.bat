d1b85fcf6e08045eb28c2803deceb5448f9b92b6
Xuefeng Zhou <zxfrocket@qq.com>
2018-01-01 11:59:24 +0800

    add update scripts for beijing_kc_new.

---
+++ tools/update.beijing.bat
@@ -0,0 +1,15 @@
+%~d0
+cd %~dp0
+::compile
+for %%f in (RcuClear RcuOrder RcuPoll) do call .\scripts\update1.bat %%f
+set packpath=com\dlrtie\rcu\
+mkdir ..\RcuService\RcuAll\lib\%packpath%
+copy ..\RcuProcess\lib\*.jar ..\RcuService\RcuAll\lib\ /y
+copy ..\RcuProcess\lib\*.dll ..\RcuService\RcuAll\lib\ /y
+copy ..\RcuProcess\src\com\dlrtie\rcu\*.java ..\RcuService\RcuAll\lib\%packpath% /y
+.\jdk\bin\javac.exe -classpath ..\RcuService\RcuAll\lib\mysql.jar;..\RcuService\RcuAll\lib\RXTXcomm.jar -encoding utf-8 ..\RcuService\RcuAll\lib\%packpath%*.java
+del ..\RcuService\RcuAll\lib\%packpath%*.java
+::because we have know the Main-Class in wrapper.conf, we need not add mainfest list when package
+.\jdk\bin\jar.exe -cvfM ..\RcuService\RcuAll\lib\RcuAll.jar -C ..\RcuService\RcuAll\lib\ com
+for %%f in (RcuClear RcuOrder RcuPoll) do call .\scripts\update2.bat %%f ..\RcuService\RcuAll\lib\RcuAll.jar
+rmdir /s /Q ..\RcuService\RcuAll /S
