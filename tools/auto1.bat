204a035974b0f062509e99653ce59dbd2439aff4
Xuefeng Zhou <zxfrocket@qq.com>
2018-08-18 06:31:53 +0800

    update kc auto script

--- tools/auto1.bat
+++ tools/auto1.bat
@@ -1,3 +1,6 @@
+net stop RcuAuto
+net stop RcuMode
+net stop RcuOpera
 net stop RcuPoll
 net stop RcuOrder
 net start RcuClear
--- tools/auto2.bat
+++ tools/auto2.bat
@@ -1,3 +1,6 @@
 net stop RcuClear
 net start RcuOrder
+net start RcuAuto
+net start RcuMode
+net start RcuOpera
 net start RcuPoll
